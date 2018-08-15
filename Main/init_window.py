8a732e818379d3a284536e7c0b77314b21758b77
Szwank-PC\Szwank <c@ijk.pl>
2018-08-15 10:35:48 +0200

    Commmit przed przeniesieniem doku okna inicjalizującego do init_window

--- Main/init_window.py
+++ Main/init_window.py
@@ -11,15 +11,16 @@ class InitWindow:
         self.window_handle = GraphWin(self.__WINDOW_NAME, wight, height) #zainicjowanie okna
 
     def wait_to_press_key(self, key):
-        '''Czekaj aż zostanie wciśnięty klawisz 'key' '''
+        """Czekaj aż zostanie wciśnięty klawisz 'key' """
 
         key_pressed = self.window_handle.checkKey()
         while key_pressed != key:
             key_pressed = self.window_handle.checkKey()
 
     def get_input_massage(self, entry_position, font_size=5):
-        '''Pobierz wiadomość i potwierdz wiadomość
-        okno do pobrania wiadomości zostanie wyświetlone w punkcie 'entry_position'(podany przez klase Point) '''
+        """Pobierz wiadomość i potwierdz wiadomość
+        okno do pobrania wiadomości zostanie wyświetlone w punkcie 'entry_position'(podany przez klase Point)
+        """
 
         input_message = Entry(entry_position, font_size)
         input_message.draw(self.window_handle)
@@ -31,12 +32,16 @@ class InitWindow:
         return input_message.getText()
 
     def display_message(self, message):
-        '''Pokarz wiadomość message klasy Text'''
+        """Pokarz wiadomość message klasy Text
+        """
+
         message.draw(self.window_handle)
 
     def hide_message(self, message):
-        '''Ukryj wiadokość klasy Text'''
-        message.undraw(self.window_handle)
+        """Ukryj wiadokość klasy Text
+        """
+
+        message.undraw()
 
     def get_wight(self):
         return self.wight
@@ -44,3 +49,6 @@ class InitWindow:
     def get_height(self):
         return self.height
 
+    def ask_about_the_size_of_the_board
+        """Tworzy okno w którym pyta użytkownika o wymiary głównej planszy
+        """
--- Main/main.py
+++ Main/main.py
@@ -3,6 +3,7 @@ import time
 from world import World
 from init_window import InitWindow
 
+
 def main():
     init_window = InitWindow()
 
@@ -10,11 +11,10 @@ def main():
     message = Text(display_message_point, 'Podaj ilość pól szerokości gry:')
     init_window.display_message(message)
     input_message = init_window.get_input_massage(Point(display_message_point.getX() + len(message.getText()) * 4.5,
-                                                  display_message_point.getY()))
+                                                        display_message_point.getY()))
     init_window.hide_message(message)
 
-
-    while str.isdigit(input_message) is False: #odpalenie pętli jeżeli string nie zawierał tylko cyfr
+    while str.isdigit(input_message) is False:  # odpalenie pętli jeżeli string nie zawierał tylko cyfr
 
         message.setText('Wiadomość zawierała znaki nie będące cyframi! Wciśnij enter by kontynuować')
         init_window.wait_to_press_key('Return')
@@ -23,7 +23,6 @@ def main():
         input_message = init_window.get_input_massage(Point(display_message_point.getX() + len(message.getText()) * 4.5,
                                                             display_message_point.getY()))
         message.undraw()
-
 
     print(input)
 
@@ -39,8 +38,3 @@ def main():
 
 
 main()
-
-
-
-
-
