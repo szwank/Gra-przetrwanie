62f6fc94913c8ca865bb1fbe66966ffafcc7c121
Szwank-PC\Szwank <c@ijk.pl>
2018-08-15 10:54:08 +0200

    Do ogladnięcia dla Adriana

--- Main/init_window.py
+++ Main/init_window.py
@@ -8,7 +8,7 @@ class InitWindow:
     def __init__(self, wight=400, height=200):
         self.wight = wight
         self.height = height
-        self.window_handle = GraphWin(self.__WINDOW_NAME, wight, height) #zainicjowanie okna
+        self.window_handle = GraphWin(self.__WINDOW_NAME, wight, height)  # zainicjowanie okna
 
     def wait_to_press_key(self, key):
         """Czekaj aż zostanie wciśnięty klawisz 'key' """
@@ -49,6 +49,25 @@ class InitWindow:
     def get_height(self):
         return self.height
 
-    def ask_about_the_size_of_the_board
+    def ask_about_the_size_of_the_board(self):
         """Tworzy okno w którym pyta użytkownika o wymiary głównej planszy
         """
+
+        display_message_point = Point(self.get_wight() / 2, self.get_height() / 2)
+        message = Text(display_message_point, 'Podaj ilość pól szerokości gry:')
+        self.display_message(message)
+        input_message = self.get_input_massage(Point(display_message_point.getX() + len(message.getText()) * 4.5,
+                                                     display_message_point.getY()))
+        self.hide_message(message)
+
+        while str.isdigit(input_message) is False:  # odpalenie pętli jeżeli string nie zawierał tylko cyfr
+
+            message.setText('Wiadomość zawierała znaki nie będące cyframi! Wciśnij enter by kontynuować')
+            self.wait_to_press_key('Return')
+
+            self.display_message(message)
+            input_message = self.get_input_massage(Point(display_message_point.getX() + len(message.getText()) * 4.5,
+                                                         display_message_point.getY()))
+            message.undraw()
+
+        return (input_message)
